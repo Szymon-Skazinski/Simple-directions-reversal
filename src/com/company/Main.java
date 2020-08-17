b5f9a7acae54cef25af4f8e392c90b148794d94f
Szymon Skazinski <szyskazi@gmail.com>
2020-08-17 18:43:41 +0200

    some refactor and methods extract

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,39 @@ public class Main {
 
 class Solution {
     public static String[] solve(String[] arr) {
-        String[][] invertedDirectionRoadArray = new String[arr.length][2];
+        String[][] invertedStepsArray = stepsInversion(arr);
+
+        String[] resolve = directionInversion(arr, invertedStepsArray);
+
+        System.out.println(resolve);
+        return resolve;
+    }
+
+    private static String[] directionInversion(String[] arr, String[][] invertedStepsArray) {
+        String[] resolve = new String[arr.length];
+
+        for ( int i = 0; i < invertedStepsArray.length ; i++) {
+
+            String direction = "";
+
+            if (i > 0){
+                direction = invertedStepsArray[i-1][0];
+            }
+
+            if (i == 0){
+                resolve[0] = "Begin on " + invertedStepsArray[i][1];
+            } else if (direction.equalsIgnoreCase("Left")){
+                resolve[i] = "Right on " + invertedStepsArray[i][1];
+            } else {
+                resolve[i] = "Left on " + invertedStepsArray[i][1];
+            }
+
+        }
+        return resolve;
+    }
+
+    private static String[][] stepsInversion(String[] arr) {
+        String[][] invertedStepsArray = new String[arr.length][2];
 
         for (int i = arr.length - 1; i >= 0; i--) {
             int j = Math.abs(i - arr.length + 1);
@@ -26,36 +58,13 @@ class Solution {
             String[] stepWordsArray = step.split(" ");
             String direction = stepWordsArray[0];
 
-            String[] roadNameArray = Arrays.copyOfRange(stepWordsArray,2, stepWordsArray.length);
-            String roadName = String.join(" ",roadNameArray);
+            String[] roadNameArray = Arrays.copyOfRange(stepWordsArray, 2, stepWordsArray.length);
+            String roadName = String.join(" ", roadNameArray);
 
-            invertedDirectionRoadArray[j][0] = direction;
-            invertedDirectionRoadArray[j][1] = roadName;
+            invertedStepsArray[j][0] = direction;
+            invertedStepsArray[j][1] = roadName;
         }
-
-        String[] resolve = new String[arr.length];
-
-        for ( int i = 0; i < invertedDirectionRoadArray.length ; i++) {
-
-            String direction = "";
-
-            if (i > 0){
-                direction = invertedDirectionRoadArray[i-1][0];
-            }
-
-            if (i == 0){
-                resolve[0] = "Begin on " + invertedDirectionRoadArray[i][1];
-            } else if (direction.equalsIgnoreCase("Left")){
-                resolve[i] = "Right on " + invertedDirectionRoadArray[i][1];
-            } else {
-                resolve[i] = "Left on " + invertedDirectionRoadArray[i][1];
-            }
-
-        }
-
-        System.out.println(resolve);
-        return resolve;
-
+        return invertedStepsArray;
     }
 }
 
